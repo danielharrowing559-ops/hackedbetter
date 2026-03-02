2b56075eafbbcb251a1e17f873667146455faae2
kaperdan <daniel.harrowing.559@accesscreative.ac.uk>
2026-03-02 11:21:12 +0000

    son

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
