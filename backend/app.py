43da45bc37327561a88658b9602f22a53a8505bb
Your Name <you@example.com>
2026-03-04 12:36:09 +0000

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, request, jsonify
+import uuid
+from flask import Flask, request, jsonify, session
+
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -8,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-
+app.secret_key = os.environ.get("SECRET_env")
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_PUBLISHABLE_KEY")
 
@@ -26,10 +28,10 @@ def sign_up():
     password = data.get("password")
 
     if not email or not password:
-        print("1")
         return jsonify({"error": "Email and password required"}), 400
 
     try:
+        # Check if email exists
         email_check = (
             supabase.table("users")
             .select("email")
@@ -38,25 +40,26 @@ def sign_up():
         )
 
         if email_check.data:
-            print("2")
             return jsonify({"error": "Email already exists"}), 400
 
-        result = (
-            supabase.table("users")
-            .insert({"email": email, "password": password, "name": name})
-            .execute()
-        )
+        # Create session token
+        session_token = str(uuid.uuid4())
 
-        if result.data:
-            return jsonify({"message": "Signup successful", "data": result.data}), 200
-        
-        print("3")
-        return jsonify({"error": "Signup failed"})
+        # Insert user
+        supabase.table("users").insert({"email": email,
+            "password": password,
+            "name": name
+        }).execute()
+        id = supabase.table("users").select("id").eq("email", email).execute()
+        session["id"] = id.data[0]["id"]
+        return jsonify({}), 200
 
+        return jsonify({"error": "Signup failed"}), 400
 
     except Exception as e:
         print(e)
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 400
+
 
 
 @app.route("/getpets", methods=["GET"])
@@ -74,6 +77,25 @@ def get_pets():
         print(e)
         return jsonify({"error": str(e)}), 400
 
+
+
+@app.route("/selectpet", methods=["POST"])
+def select_pet():
+    data = request.get_json() or {}
+    session = data.get("session")
+    pet_id = data.get("pet_id")
+
+    if not session or not pet_id:
+        return jsonify({"error": "session and pet_id required"}), 400
+
+    try:
+
+        supabase.table("users") .update({"pet_id": pet_id}).eq("id", session["id"]) .execute()
+        return jsonify({}), 200
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
